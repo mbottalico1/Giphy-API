4e7b3464889f5ba31a4894b0fbad2c3ebd0b6ffe
Rosenberg-Contractor, Keith (CORP) <Keith.Rosenberg-Contractor@adp.com>
2017-05-01 19:58:36 -0400

    retab and organize code neatly, add document ready

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,42 +1,40 @@
-//find a way to make this work in a different way. Instead add this attribute to the variable topics and get rid of buttons in html, create them in javascript?
-var topics = ['Michael Scott', 'Dwight Schrute', 'Shane Walsh', 'Rick Grimes'];
+$(document).ready(function () {
+	//find a way to make this work in a different way. Instead add this attribute to the variable topics and get rid of buttons in html, create them in javascript?
+	var topics = ['Michael Scott', 'Dwight Schrute', 'Shane Walsh', 'Rick Grimes'];
 
-$('#Michael').append(topics[0]);
-$('#Dwight').append(topics[1]);
-$('#Shane').append(topics[2]);
-$('#Rick').append(topics[3]);
+	$('#Michael').append(topics[0]);
+	$('#Dwight').append(topics[1]);
+	$('#Shane').append(topics[2]);
+	$('#Rick').append(topics[3]);
 
-$('button').on('click', function() {
+	$('button').on('click', function() {
+		var person = $(this).attr("button");
 
-var person = $(this).attr("button");
-//might need new api key. Most likely
-var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + person + '&api_key=dc6zaTOxFJmzC&limit=10';
+		//might need new api key. Most likely
+		var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + person + '&api_key=dc6zaTOxFJmzC&limit=10';
 
-$.ajax({
-	url: queryURL,
-	method: 'GET'
-})
-.done(function(response) {
-	var results = response.data;
+		$.ajax({
+			url: queryURL,
+			method: 'GET'
+		})
+		.done(function(response) {
+			var results = response.data;
 
-	for (var i = 0; i < results.length; i++) {
-		    var gif = $('<div class="item">');
+			for (var i = 0; i < results.length; i++) {
+				var gif = $('<div class="item">');
 
-            var rating = results[i].rating;
+				var rating = results[i].rating;
 
-            var p = $('<p>').text('Rating: ' + rating);
+				var p = $('<p>').text('Rating: ' + rating);
 
-            var image = $('<img>');
-            image.attr('src', results[i].images.fixed_height.url);
+				var image = $('<img>');
+				image.attr('src', results[i].images.fixed_height.url);
 
-            gif.prepend(p);
-            gif.prepend(image);
+				gif.prepend(p);
+				gif.prepend(image);
 
-            $('#GoT-buttons').prepend(gif);
-	}
-})
-
-})
-
-
-
+				$('#GoT-buttons').prepend(gif);
+			}
+		})
+	});
+});
